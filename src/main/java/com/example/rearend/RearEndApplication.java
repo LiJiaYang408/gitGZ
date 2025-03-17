e1664f1ecb3fd4b62811ff832afe0bbbc6721e10
LiJiaYang408 <2196683070@qq.com>
2025-03-17 16:58:45 +0800

    下午第二版

--- src/main/java/com/example/rearend/RearEndApplication.java
+++ src/main/java/com/example/rearend/RearEndApplication.java
@@ -1,9 +1,11 @@
 package com.example.rearend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.rearend.mapper")
 public class RearEndApplication {
 
     public static void main(String[] args) {
