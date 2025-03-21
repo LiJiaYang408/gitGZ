ecd3e1b5cba8e2744a7ed1e667c42ccbcdfa765f
LiJiaYang408 <2196683070@qq.com>
2025-03-21 13:11:57 +0800

    加上sql文件

--- src/main/java/com/example/rearend/config/SecurityConfig.java
+++ src/main/java/com/example/rearend/config/SecurityConfig.java
@@ -41,9 +41,8 @@ public class SecurityConfig {
                 // 配置请求权限
                 .authorizeHttpRequests(authorize -> authorize
                         // 允许公开访问的路径
-                        .requestMatchers("/api/auth/**","/table/*").permitAll() // 登录和注册接口
-                        .requestMatchers("/public/**").permitAll()   // 公开资源
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 文档
+                        .requestMatchers("/api/auth/*","/table/*").permitAll() // 登录和注册接口
+//                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 文档
 
                         // 需要特定角色的路径
                         .requestMatchers("/admin/**").hasRole("ADMIN") // 管理员接口
